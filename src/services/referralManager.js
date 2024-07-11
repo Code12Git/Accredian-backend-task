3e8f9de645d8c5d4ea642822ab596b1da8d1c99f
Code12Git <saxenasaksham46@gmail.com>
2024-07-11 12:54:23 +0530

    Fixing bugs related to email servicec

--- src/services/referralManager.js
+++ src/services/referralManager.js
@@ -5,16 +5,16 @@ const prisma = require('../plugins/connection')
 const sendReferrerEmail = require('./EmailManager')
 
 const create = async(body) => {
-    const { referrerName, referrerEmail, refereeName, refereeEmail } = body;
+    const { referrerName, referrerEmail, refereeName, refereeEmail, courseName } = body;
     try{
-        if(_.isEmpty(referrerName) || _.isEmpty(referrerEmail) || _.isEmpty(refereeName) || _.isEmpty(refereeEmail)){
+        if(_.isEmpty(referrerName) || _.isEmpty(referrerEmail) || _.isEmpty(refereeName) || _.isEmpty(refereeEmail) || _.isEmpty(courseName)){
             const error = {...BAD_REQUEST , message:"All fields are required"}
             throw new AppError(error.code, error.message,error.statusCode)
         }
         const referral = await prisma.referral.create({
-            data: { referrerName , referrerEmail , refereeName , refereeEmail }
+            data: { referrerName , referrerEmail , refereeName , refereeEmail , courseName }
         })
-        sendReferrerEmail( referrerEmail, refereeName )
+        await sendReferrerEmail( refereeName ,referrerName, courseName,refereeEmail )
         return referral;
     } catch(err){
         throw err;
